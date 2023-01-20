b9ca078dee4d7ecf7e8eca15055440cac89d9a9a
Minh <94094632+ImDoneWithThisUsername@users.noreply.github.com>
2023-01-20 22:50:09 +0700

    authentication and create frontend folder

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { registeredUser, loginUser, getMe } = require('../controllers/userController')
+
+const {protect} = require('../middleware/authMiddleware')
+
+router.post('/', registeredUser)
+router.post('/login', loginUser)
+router.get('/me', protect, getMe)
+
+module.exports = router
